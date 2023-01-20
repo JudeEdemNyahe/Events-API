a081ff116f05809ce9dea1e39bd1148b33031aaa
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2023-01-20 10:30:37 +0000

    database concepts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import typeOrmConfig from './config/typeOrm.Config';
+import typeOrmConfigProd from './config/typeOrm.Config.prod';
+
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeOrmConfig],
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== 'production'
+          ? typeOrmConfig
+          : typeOrmConfigProd,
+    }),
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
